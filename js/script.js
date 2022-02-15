e4facfc638f8d002b5d1d2b23c32c3dbe5a17d99
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-14 22:11:39 -0800

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-document.getElementById('sixteenGB')
+document.getElementById('sixteenGB').addEventListener('click', function(){
+    const memoryCost = document.getElementById("memory-cost");
+    memoryCost.innerText = 200;
+    // memoryCost.innerText = parseFloat(memoryCost.innerText) + 200;
+    const bestPrice = document.getElementById("best-price").innerText;
+    const memoryPrice = document.getElementById("memory-cost").innerText;
+    const storagePrice = document.getElementById("storage-cost").innerText;
+    const deliveryPrice = document.getElementById("delivery-cost").innerText;
+
+    const totalPrice = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
+
+    const total = document.getElementById("total-price");
+    total.innerText = totalPrice;
+    
+})
